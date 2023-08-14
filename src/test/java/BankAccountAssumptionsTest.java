31fdd5159ebff33b1db9cd5e50bd21a5fddfab4e
Hamzah Contreras <h.contreras9@outlook.com>
2023-08-14 00:16:40 -0500

    Writing Basic Tests with JUnit

---
+++ src/test/java/BankAccountAssumptionsTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assumptions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountAssumptionsTest {
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void testActive0() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+        assumeTrue(bankAccount != null);
+        assertTrue(bankAccount.isActive());
+    }
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void testActive1() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+        assumeFalse(bankAccount == null);
+        assertTrue(bankAccount.isActive());
+    }
+
+    @Test
+    @DisplayName("Test activation account after creation")
+    public void testActive2() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+        assumingThat(bankAccount == null, () -> assertTrue(bankAccount.isActive()));
+    }
+}
